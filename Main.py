814bbc289c6e8b068c8e6b1052cf9042dbebb40e
URK21CS3014 <109951873+URK21CS3014@users.noreply.github.com>
2022-08-12 21:25:36 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -28,13 +28,18 @@ class LinkedList:
         Insert node at end of the list
         :param data: integer data that will be used to create a node
         """
-        # Write code here
+        new_node=Node(data)
+        new_node.next = self.head
+        self.head = new_node
 
     def status(self):
         """
         It prints all the elements of list.
         """
-        # write code here
+        temp = self.head
+        while(temp):
+            print temp.data,
+            temp = temp.next
 
 
 class Solution:
@@ -47,7 +52,28 @@ class Solution:
         :param second_list: Linkedlist with non-negative integers
         :return: returns the sum as a linked list
         """
-        # Write code here
+        prev=None
+        temp = None
+        carry = 0
+        while(first is not None or second is not None):
+            fdata = 0 if first is None else first.data
+            sdata = 0 if second is None else second.data
+            Sum = carry + fdata + sdata
+            carry = 1 if Sum >= 10 else 0
+            Sum = Sum if Sum < 10 else Sum % 10
+            temp = Node(Sum)
+            if self.head is None:
+                    self.head = temp
+            else:
+                    prev.next = temp
+            prev = temp
+            if first is not None:
+                    first = first.next
+            if second is not None:
+                    second = second.next
+  
+          if carry > 0:
+            temp.next = Node(carry)
         
         
 
