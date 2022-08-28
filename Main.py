0138fb8a647004042b2a10b6eeae28cb7fac4fa3
URK21CS3014 <109951873+URK21CS3014@users.noreply.github.com>
2022-08-28 21:53:35 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,22 +1,17 @@
 from typing import Optional
 
-
 class Node:
-    """
-    Provide necessary documentation
-    """
+
     def __init__(self, data=None, next=None):
         """
-        Provide necessary documentation
+        Initialises the Node with given attributes
         """
         self.data = data
         self.next = next
 
 
 class LinkedList:
-    """
-    Provide necessary documentation
-    """
+
     def __init__(self):
         """
         Initialize the head
@@ -24,56 +19,43 @@ class LinkedList:
         self.head = None
 
     def insert_at_end(self, data):
-        """
-        Insert node at end of the list
-        :param data: integer data that will be used to create a node
-        """
-        new_node=Node(data)
-        new_node.next = self.head
-        self.head = new_node
+        new = Node(data, None)
+        current = self.head
+        if current is None:
+            self.head = new
+        else:
+            while current.next is not None:
+                current = current.next
+            current.next = new
 
     def status(self):
-        """
-        It prints all the elements of list.
-        """
-        temp = self.head
-        while(temp):
-            print (temp.data)
-            temp = temp.next
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(elements)
 
 
 class Solution:
-    """
-    Provide necessary documentation
-    """
+
     def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
-        """
-        :param first_list: Linkedlist with non-negative integers
-        :param second_list: Linkedlist with non-negative integers
-        :return: returns the sum as a linked list
-        """
-        prev=None
-        temp = None
-        carry = 0
-        while(first is not None or second is not None):
-            fdata = 0 if first is None else first.data
-            sdata = 0 if second is None else second.data
-            Sum = carry + fdata + sdata
-            carry = 1 if Sum >= 10 else 0
-            Sum = Sum if Sum < 10 else Sum % 10
-            temp = Node(Sum)
-            if self.head is None:
-                    self.head = temp
-            else:
-                    prev.next = temp
-            prev = temp
-            if first is not None:
-                    first = first.next
-            if second is not None:
-                    second = second.next
-  
-        if carry > 0:
-            temp.next = Node(carry)
+        result = self.get_num(first_list) + self.get_num(second_list)
+        sum_list = LinkedList()
+        for digit in list(map(int, str(result)[::-1])):
+            sum_list.insert_at_end(digit)
+        return sum_list
+
+    def get_num(self, l: Optional[LinkedList]) -> int:
+        curr = l.head
+        if curr is None:
+            return 0
+        num = ""
+        while curr is not None:
+            num = str(curr.data) + num
+            curr = curr.next
+        return int(num)
+
         
         
 
